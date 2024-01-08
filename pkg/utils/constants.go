2a443465994aab76c2775cd9ebdf208a874ef913
thelicato <thelicato@users.noreply.github.com>
2024-01-09 00:48:00 +0100

    release: bump version to 0.1.7

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -27,7 +27,7 @@ var ANSICodes = map[string]string{
 	"WhiteBackground":   "\033[47m",
 }
 
-const Version = "0.1.6"
+const Version = "0.1.7"
 const MainDirName = ".secbutler"
 const PayloadsDirName = "payloads"
 const ToolsDirName = "tools"
